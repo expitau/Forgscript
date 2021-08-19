5bd4bbcadbb003a014ee8abd8f09b4e36058fbab
expitau-dev <expitau@gmail.com>
2021-08-19 11:51:28 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# Forglang
+
+## Documentation
+Control flow begins at the 'forg', or @ symbol. Every iteration of the program, the forg will jump to a new command given by:
+
+ - (Current line number) / 2 if the line number is even.
+  - 1 + (Current line number) * 3 if the line number is odd.
+
+Additionally, if the forg lands in water, he will continue swimming in the direction he was travelling until he reaches another command or a duck lets him on its back. Note that in the case that the map contains no water or ducks, the forg evaluates the [Collatz Conjecture](https://en.wikipedia.org/wiki/Collatz_conjecture).
+
+The forg remembers things using its brain, which acts like a stack. By default, every cell in the stack is initialized to zero. The `+` and `-` commands increment or decrement the value of one of the memory cells in the stack.
+
+## Commands
+
+### Control flow
+`o` is a lillypad. The forg will sit here for a specified memory cell's period of time (none by default)
+
+`d` a duck. Will let the forg sit on his back if the forg has a nonzero number in the memory cell specified. The duck acts as a lillypad if forg sits on his back, or as water otherwise.
+
+`p` upside-down duck. Will let the forg sit on his back if the forg has a zero at the memory cell specified.
+
+`~` Water, the forg will swim past here without halting.
+
+`@` is the forg's starting location. If there are multiple `@`s in the program, all but the first fall asleep and act as lillypads. The forg always starts on a lillypad.
+
+`z` Forg bedtime. Forg will slep and program will stop. 
+
+### IO
+
+`*` a bug. The forg will eat one character from stdin and set the memory cell specified
+
+`^` The forg will ribbit one character to stdout from the memory cell specified
+
+### Operations
+
+`+` The forg will increment the memory cell specified.
+
+`-` The forg will decrement the memory cell specified.
+
+
+
+
+
+## Example
+
+### Code
+The following program will output the sum of two numbers. (line numbers included for clarity)
+```
+1  |  @
+2  |  ~
+3  |  ~
+4  |  ~
+5  |  *1
+6  |  ~
+7  |  -1
+8  |  ~
+9  |  p1
+10 |  ~
+11 |  ~
+12 |  ~
+13 |  ~
+14 |  z
+15 |  ~
+16 |  ~
+17 |  ~
+18 |  *2
+19 |  ~
+20 |  ~
+21 |  ~
+22 |  +2
+23 |  ~
+24 |  ~
+25 |  ~
+26 |  ~ 
+27 |  ~
+28 |  ^2
+```
+Forglang ignores whitespace, so equivalent code would be
+```
+@~~~*1~-1~p1~~~~z~~~*2~~~+2~~~~~^2
+```
+
+### Explanation
+Control flow starts at the forg on line 1, this is an odd number so the forg jumps to line 4 and then swims to line 5. `*1` reads one number into forg memory cell 1 (lets say, the number 3). Forg then jumps to line 16 and swims to line 18. `*2` reads another number into cell 2 (lets say this number is 5). 
+
+18 is even, so the forg then hops back to line 9. Because memory cell 1 is not zero, `p1` (the upside down duck) decides to not let forg onto its back. Forg swims to line 7, and decrements memory cell 1 (now 2). Forg jumps to line 22, and increments memory cell 2 (now 6). Forg jumps back to line 11 and swims to the upside-down duck on line 9. Memory cell 1 still isn't zero, so forg continues swimming until he once again reaches line 7. 
+
+He continues decrementing cell 1 and incrementing cell 2 until cell 1 is zero and cell 2 is eight. Now, forg swims to the upside-down duck and (because cell 1 is zero) the duck lets him on his back, so forg can jump all the way to line 28, where he outputs 8 to stdout. He finally jumps to line 14, where he falls asleep and the program terminates.
---
+++ forglang.cc
@@ -0,0 +1,211 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+class Command
+{
+private:
+   char command;
+   int data;
+   int line;
+
+public:
+   Command(char c, int d, int l) : command{c}, data{d}, line{l} {};
+   operator string()
+   {
+      string out;
+      out += to_string(line + 1);
+      if (line + 1 < 10)
+      {
+         out += " ";
+      }
+      if (line + 1 < 100)
+      {
+         out += " ";
+      }
+      out += "| ";
+      out += command;
+      out += to_string(data);
+      out += '\n';
+      return out;
+   }
+   char get_command() { return command; }
+   int get_data() { return data; }
+   int get_line() { return line; }
+   int get_jump()
+   {
+      if (line % 2 == 0)
+      {
+         return line / 2;
+      }
+      else
+      {
+         return 1 + line * 3;
+      }
+   }
+};
+
+class OutOfBoundsException
+{
+};
+
+class Forg
+{
+private:
+   vector<Command> map;
+   int line;
+   std::map<int, int> memory;
+
+public:
+   Forg(vector<Command> map) : map{map}, line{1} {};
+   Forg(vector<Command> map, int line) : map{map}, line{line} {};
+   void execute()
+   {
+      switch (map[line].get_command()){
+         case '*':
+            cin >> memory[map[line].get_data()];
+            break;
+         case '^':
+            cout << memory[map[line].get_data()] << endl;
+            break;
+         case '+':
+            memory[map[line].get_data()]++;
+            break;
+         case '-':
+            memory[map[line].get_data()]--;
+            break;
+         default:
+            break;
+      }
+   }
+   void jump()
+   {
+      // cout << "From " << line + 1 << endl;
+      if ((line + 1) % 2 == 0)
+      {
+         line = (line + 1) / 2 - 1;
+         // cout << "Jumped to " << line + 1 << endl;
+         swim(-1);
+      }
+      else
+      {
+         line = 3 * (line + 1);
+         // cout << "Jumped to " << line + 1 << endl;
+         swim(1);
+      }
+      // cout << "Swam to " << line + 1 << endl;
+   }
+   void swim(int dir)
+   {
+      if (line < 0 || line >= map.size() || map[line].get_command() == 'z')
+      {
+         throw OutOfBoundsException{};
+      }
+
+      while (map[line].get_command() == '~' || (map[line].get_command() == 'd' && memory[map[line].get_data()] == 0) || (map[line].get_command() == 'p' && memory[map[line].get_data()] != 0))
+      {
+         line += dir;
+         if (line < 0 || line >= map.size() || map[line].get_command() == 'z')
+         {
+            throw OutOfBoundsException{};
+         }
+      }
+   }
+};
+
+vector<Command> parse(istream *input_stream)
+{
+   vector<Command> out;
+   stringstream in;
+   stringstream in2;
+   char c;
+   while (true)
+   {
+      *input_stream >> c;
+      if ((*input_stream).eof())
+         break;
+      in << c;
+      in2 << c;
+   }
+   in.clear();
+   in2.clear();
+
+   char cdump;
+   char ctest;
+   int i;
+   int idump;
+
+   in2 >> cdump;
+   while (true)
+   {
+      // Read command
+      in >> c;
+
+      // Test if next char is a number, if so read it
+      in2 >> ctest;
+      if (isdigit(ctest))
+      {
+         in >> i;
+         while (isdigit(ctest) && !in2.eof())
+         {
+            in2 >> ctest;
+         }
+      }
+      else
+      {
+         i = 0;
+      }
+      out.push_back(Command(c, i, out.size()));
+
+      if (in2.eof())
+      {
+         break;
+      }
+   }
+   return out;
+}
+
+int main(int argc, char const *argv[])
+{
+   vector<Command> commands;
+   if (argc == 2)
+      commands = parse(new ifstream(argv[1]));
+   else
+   {
+      cout << "No input file specified" << endl;
+      return 0;
+   }
+
+   int forgl = 0;
+
+   for (auto c : commands)
+   {
+      if (c.get_command() == '@')
+      {
+         forgl = c.get_line();
+         break;
+      }
+   }
+
+   Forg f(commands, forgl);
+
+   while (true)
+   {
+      try
+      {
+         f.execute();
+         f.jump();
+      }
+      catch (OutOfBoundsException)
+      {
+         cout << "Done!" << endl;
+         break;
+      }
+   }
+
+   return 0;
+}
